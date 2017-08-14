6208b1a5f054fb92f4d8c673fa7a9ceeb1ef95ca
Athan Clark <athan.clark@gmail.com>
2017-08-13 21:40:09 -0600

    basic bindings

---
+++ src/Electron.purs
@@ -0,0 +1,21 @@
+module Electron (OpenWindowParams, ELECTRON, openWindow) where
+
+import Prelude
+import Control.Monad.Eff (kind Effect, Eff)
+import Control.Monad.Eff.Uncurried (EffFn1)
+
+
+
+type OpenWindowParams =
+  { file :: String -- Relative
+  , width :: Int
+  , height :: Int
+  }
+
+foreign import data ELECTRON :: Effect
+
+foreign import openWindowImpl :: forall eff. EffFn1 (electron :: ELECTRON | eff) OpenWindowParams Unit
+
+
+openWindow :: forall eff. OpenWindowParams -> Eff (electron :: ELECTRON | eff) Unit
+openWindow = runEffFn1 openWindowImpl
