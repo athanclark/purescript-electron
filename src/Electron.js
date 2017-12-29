aba88c7f4f3a234642d0d331f76f9323dbe50696
Athan Clark <athan.clark@gmail.com>
2017-12-28 19:38:37 -0700

    electron quit

--- src/Electron.js
+++ src/Electron.js
@@ -60,7 +60,7 @@ exports.openWindowImpl = function openWindowImpl (ps) {
 
 
   app.on('before-quit', function (e) {
-    ps.beforeQuit(e);
+    ps.beforeQuit(e, function quitImpl () {app.quit();});
   });
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
--- src/Electron.purs
+++ src/Electron.purs
@@ -6,7 +6,7 @@ import Electron.Types as Types
 import Prelude
 import Data.Argonaut (Json)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Uncurried (EffFn1, runEffFn1, mkEffFn1)
+import Control.Monad.Eff.Uncurried (EffFn1, EffFn2, runEffFn1, mkEffFn1, mkEffFn2)
 
 import React (ReactElement, createElementTagName)
 
@@ -25,8 +25,8 @@ foreign import openWindowImpl :: forall eff. EffFn1 (electron :: ELECTRON | eff)
                                     , whenLoaded :: EffFn1 (electron :: ELECTRON | eff)
                                                       { send :: EffFn1 (electron :: ELECTRON | eff) {channel :: String, message :: Json} Unit
                                                       } Unit
-                                    , beforeQuit :: EffFn1 (electron :: ELECTRON | eff)
-                                                      (Event (electron :: ELECTRON | eff)) Unit
+                                    , beforeQuit :: EffFn2 (electron :: ELECTRON | eff)
+                                                      (Event (electron :: ELECTRON | eff)) (Eff (electron :: ELECTRON | eff) Unit) Unit
                                     } Unit
 
 openWindow :: forall eff. { file :: String
@@ -35,13 +35,15 @@ openWindow :: forall eff. { file :: String
                           , devTools :: Boolean
                           , whenLoaded :: { send :: {channel :: String, message :: Json} -> Eff (electron :: ELECTRON | eff) Unit
                                           } -> Eff (electron :: ELECTRON | eff) Unit
-                          , beforeQuit :: Event (electron :: ELECTRON | eff) -> Eff (electron :: ELECTRON | eff) Unit
+                          , beforeQuit :: { preventDefault :: Eff (electron :: ELECTRON | eff) Unit
+                                          , quit :: Eff (electron :: ELECTRON | eff) Unit
+                                          } -> Eff (electron :: ELECTRON | eff) Unit
                           } -> Eff (electron :: ELECTRON | eff) Unit
 openWindow {file,width,height,devTools,whenLoaded,beforeQuit} =
   runEffFn1 openWindowImpl
     { file, width, height, devTools
     , whenLoaded: mkEffFn1 \{send} -> whenLoaded {send: runEffFn1 send}
-    , beforeQuit: mkEffFn1 beforeQuit
+    , beforeQuit: mkEffFn2 \{preventDefault} quit -> beforeQuit {preventDefault,quit}
     }
 
 
